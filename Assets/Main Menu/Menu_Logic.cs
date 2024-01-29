adfe34a58c9a048a4ce6d5fca06d4271b1036ada
KetamineBoulette <kirilljj5@gmail.com>
2024-01-29 21:43:56 +0100

    Added Title Scene & Fixed Stairs

--- Assets/Main Menu/Menu_Logic.cs
+++ Assets/Main Menu/Menu_Logic.cs
@@ -1,19 +1,28 @@
 //using System.Collections;
 //using System.Collections.Generic;
 using UnityEngine;
-//using UnityEngine.SceneManagement;
+using UnityEngine.SceneManagement;
 
 public class Menu_Logic : MonoBehaviour
 {
     public GameObject canvas;
     LevelLoader fade;
+    Scene scene;
 
     void Start()
     {
         fade = canvas.GetComponent<LevelLoader>();
     }
     public void startGame() {
-        fade.LoadNextLevel();
+        if (SceneManager.GetActiveScene().buildIndex == 4)
+        {
+            SceneManager.LoadScene("Tutorial");
+        }
+        else
+        {
+            fade.LoadNextLevel();
+        }
+        
         //SceneManager.LoadScene("Tutorial");
     }
     public void QuitGame() {
